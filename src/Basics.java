937cdec864ac33371599a8df37280f55eb04f032
codedbykaif <kaif_246048@saitm.ac.in>
2025-10-06 15:51:58 +0530

    code

---
+++ src/Basics.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+    import java.util.Scanner;
+public class Basics {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        int a = 10;
+//
+//        if(a == 10){
+//            System.out.println("Hello world: ");
+//        }
+
+        int count = 1;
+        while(count != 5){
+            System.out.println(count);
+            count++;
+        }
+
+
+    }
+}
