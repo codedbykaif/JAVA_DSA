40ac3a5d8b87588ae79db4e63a65102c98e7a8a3
codedbykaif <kaif_246048@saitm.ac.in>
2025-10-07 10:23:53 +0530

    loops

---
+++ src/conditionals_loops/loops.java
@@ -0,0 +1,22 @@
+package conditionals_loops;
+import java.util.Scanner;
+public class loops {
+    public static void main(String[] args) {
+//        print numbers from 1 to 5
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+//        for(int i = 1; i<=5; i++){
+//            System.out.println(i);
+//        }
+
+//        print number from 1 to n
+        for(int i=1; i<=num; i++){
+            System.out.println(i);
+        }
+
+
+
+
+
+    }
+}
