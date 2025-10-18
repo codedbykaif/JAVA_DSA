2e62c7ae2272dddb2ae3f2e0cc764ad16b08ddcc
codedbykaif <kaif_246048@saitm.ac.in>
2025-10-18 10:44:40 +0530

    code

--- src/Switch/fruit.java
+++ src/Switch/fruit.java
@@ -5,13 +5,22 @@ public class fruit {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter name of fruit: ");
         String fruit = input.next();
-        if(fruit.equals("Mango")){
-            System.out.println("king of fruits");
-
-        }
-        if(fruit.equals("apple")){
-            System.out.println("a sweet red fruit");
-        }
+       switch (fruit) {
+           case "Mango":
+               System.out.println("king of fruits");
+               break;
+           case "Apple":
+               System.out.println("a sweet red fruit");
+               break;
+           case "Orange":
+               System.out.println("round fruit");
+               break;
+           case "Grapes":
+               System.out.println("Small fruit");
+               break;
+           default:
+               System.out.println("Please enter a valid furit");
+       }
 
     }
 }
