4722b88fed2a36a29d01a5fa68eb92e044392c78
codedbykaif <kaif_246048@saitm.ac.in>
2025-10-19 13:49:37 +0530

    code

---
+++ src/Methods/swap.java
@@ -0,0 +1,14 @@
+package Methods;
+
+public class swap {
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 32;
+//        swap numbers code
+
+         int temp = a;
+         a = b;
+         b = temp;
+        System.out.println(a + " " + b);
+    }
+}
