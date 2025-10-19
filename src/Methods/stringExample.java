3cb49083e6a954e56cf274e0e68b4e97535f71b6
codedbykaif <kaif_246048@saitm.ac.in>
2025-10-19 13:46:24 +0530

    code

--- src/Methods/stringExample.java
+++ src/Methods/stringExample.java
@@ -2,9 +2,16 @@ package Methods;
 
 public class stringExample {
     public static void main(String[] args) {
-        String message = greet();
-        System.out.println(message);
+//        String message = greet();
+//        System.out.println(message);
+        String personalized = myGreet("md kaif");
+        System.out.println(personalized);
 
+
+    }
+    static String myGreet(String name){
+        String message = "hello " + name;
+        return message;
     }
     static String greet(){
         String greeting = "how are you";
