3db70c29d909de6841de93739b74931131fc5229
codedbykaif <kaif_246048@saitm.ac.in>
2025-10-24 09:37:35 +0530

    code

---
+++ src/Methods/passingExample.java
@@ -0,0 +1,12 @@
+package Methods;
+
+public class passingExample {
+    public static void main(String[] args) {
+        String name = "md kaif" ;
+
+        greet(name);
+    }
+    static void greet(String naam){
+        System.out.println(naam);
+    }
+}
