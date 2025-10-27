2a98808fcbd5f52a25cf17a7b30b9e3795aab47c
codedbykaif <kaif_246048@saitm.ac.in>
2025-10-27 21:36:08 +0530

    code

--- src/Arrays/multiDimension.java
+++ src/Arrays/multiDimension.java
@@ -1,4 +1,5 @@
 package Arrays;
+import java.util.Arrays;
 import java.util.Scanner;
 public class multiDimension {
     public static void main(String[] args) {
@@ -18,14 +19,25 @@ public class multiDimension {
 //                {6, 7, 8, 9}   // 2nd index
 //        };
 //        System.out.println(arr2D[2][0]);
-        int[][] arr = new int[3][2];
+        int[][] arr = new int[3][3];
         System.out.println(arr.length);  // no of rows
 //        input
         for (int row = 0; row < arr.length; row++){
             for(int col = 0; col < arr[row].length; col++){
                 arr[row][col] = input.nextInt();
 
-            }
+            } }
+////        output
+//            for (int row = 0; row < arr.length; row++){
+//                for(int col = 0; col < arr[row].length; col++){
+//                    System.out.print(arr[row][col] + " ");
+//
+//                }
+//                System.out.println();
+//
+//        }
+        for(int row = 0; row < arr.length; row++){
+            System.out.println(Arrays.toString(arr[row]));
         }
 
     }
