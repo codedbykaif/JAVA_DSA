3e258ac17f0c490fabd9072330e7c884399a80ef
codedbykaif <kaif_246048@saitm.ac.in>
2025-10-27 12:26:34 +0530

    code

---
+++ src/Arrays/main.java
@@ -0,0 +1,20 @@
+package Arrays;
+
+public class main {
+    public static void main(String[] args) {
+//
+//        int[] arr = {423, 4234, 52, 523, 234};
+//
+//        System.out.println(arr[0]);
+//        System.out.println(arr[3]);
+        int[] ros;
+        ros = new int[5];
+        System.out.println(ros[1]);
+        String[] arr = new String[4];
+        System.out.println(arr[1]);
+
+
+
+    }
+}
+
