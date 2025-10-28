beba23b3c6462579ddfbd7444d6afa85112bafe6
codedbykaif <kaif_246048@saitm.ac.in>
2025-10-28 15:52:24 +0530

    code

--- src/Arrays/multiA.java
+++ src/Arrays/multiA.java
@@ -14,6 +14,7 @@ public class multiA {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 list.get(i).add(input.nextInt());
+
                 
             }
         }
