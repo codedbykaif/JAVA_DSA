0c791beb9129404f62fb96dff2ab0a06909738d4
codedbykaif <kaif_246048@saitm.ac.in>
2025-10-29 11:47:52 +0530

    code

--- src/Arrays/max.java
+++ src/Arrays/max.java
@@ -2,12 +2,34 @@ package Arrays;
 
 public class max {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4 ,5 };
-        System.out.println(max(arr));
-
+        int[] arr = {1, 2, 34, 4 ,5 };
+        System.out.println(maxRange(arr, 1, 3));
     }
-//    Imagine that array is not maximum
+
+//    work on edge cases here, like array being null or whatever
+    static int maxRange(int[] arr, int start, int end) {
+        if(end > start){
+            return -1;
+        }
+        if(arr == null){
+            return -1;
+        }
+
+        int maxVal = arr[start];
+        for (int i = start; i <= end; i++) {
+            if (arr[i] > maxVal) {
+                maxVal = arr[i];
+
+            }
+        }
+        return maxVal;
+    }
+
+    //    Imagine that array is not maximum
     static int max(int[] arr) {
+        if(arr.length == 0){
+            return -1;
+        }
         int maxVal = arr[0];
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > maxVal) {
@@ -16,4 +38,6 @@ public class max {
             }
         }
         return maxVal;
-    }}
+    }
+
+}
