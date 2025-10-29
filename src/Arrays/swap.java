a7b5cfea87a24f8ca38de627f2c023d82e1398a2
codedbykaif <kaif_246048@saitm.ac.in>
2025-10-29 12:01:36 +0530

    code

--- src/Arrays/swap.java
+++ src/Arrays/swap.java
@@ -5,9 +5,19 @@ import java.util.Arrays;
 public class swap {
     public static void main(String[] args) {
 
-        int[] arr = {1, 2, 3, 4, 5, 6, 7};
-        swap(arr, 1, 3);
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+//        swap(arr, 1, 3);
+        reverseArray(arr);
         System.out.println(Arrays.toString(arr));
+    }
+    static void reverseArray(int[] arr){
+            int start = 0;
+            int end = arr.length-1;
+            while(start < end){
+                swap(arr, start, end);
+                start++;
+                end--;
+            }
     }
     static void swap(int[] arr, int index1, int index2){
         int temp = arr[index1];
