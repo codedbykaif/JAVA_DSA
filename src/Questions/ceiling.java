8f9b1ac494b01796a8cb128862d8c22c4cfe6d93
codedbykaif <kaif_246048@saitm.ac.in>
2025-11-10 16:20:02 +0530

    code

---
+++ src/Questions/ceiling.java
@@ -0,0 +1,31 @@
+package Questions;
+
+public class ceiling {
+    public static void main(String[] args) {
+        int[] arr ={2, 3, 5, 9, 14, 16, 18};
+        int target = 15;
+        int ans = ceilling(arr, target);
+        System.out.println(ans);
+
+
+    }
+//    smallest number that is greater than target
+    static int ceilling(int[] arr, int target){
+        int start = 0;
+        int end = arr.length -1;
+        while(start <= end){
+//            find the middle element
+            int mid = start + (end - start) /2;
+            if(target < arr[mid]){
+                end = mid -1;
+            }
+            else if (target > mid) {
+                start = mid +1;
+            }
+            else{
+                return mid;
+            }
+        }
+        return start;
+    }
+}
