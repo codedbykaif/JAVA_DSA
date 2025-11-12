bc4c6bfcdefe2c1042da9f6ff45e137250d75668
codedbykaif <kaif_246048@saitm.ac.in>
2025-11-12 13:05:19 +0530

    code

---
+++ src/Questions/target.java
@@ -0,0 +1,30 @@
+package Questions;
+
+public class target {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 6, 7, 8, 10, 11, 12, 15, 20, 23, 30};
+        int target = 15;
+        int ans = search(arr, target);
+        System.out.println(ans);
+
+    }
+    static int search(int[] arr, int target){
+        int start = 0;
+        int end = arr.length -1;
+        while(start <= end){
+//            find middle element
+            int mid = start + (end - start) /2;
+            if(target <arr[mid]){
+                end = mid -1;
+            }
+            else if(target > arr[mid]){
+                start = mid + 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
