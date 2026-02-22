8fd41031c5abed2b8ef592b2d4e884bbfe62e646
chicobyte <bytechico@gmail.com>
2026-02-22 16:46:36 +0530

    commit check

---
+++ src/Revision/evenOdd.java
@@ -0,0 +1,32 @@
+package Revision;
+import java.util.Scanner;
+
+public class evenOdd {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("enter your first number: ");
+        int num1 = sc.nextInt();
+//        System.out.println("Enter your second number: ");
+//        int num2 = sc.nextInt();
+        if(num1%2==0){
+            System.out.println("the given number is even ");
+        } else{
+            System.out.println("the given number is odd ");
+        }
+
+
+
+
+
+
+
+
+
+//        divisble by 5 or not
+
+        System.out.println("Enter your first number");
+
+    }
+}
