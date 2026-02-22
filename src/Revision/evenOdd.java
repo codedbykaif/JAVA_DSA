7cd1498be0651b126797eee3be14fa2ae97319ae
chicobyte <bytechico@gmail.com>
2026-02-22 16:49:07 +0530

    code

--- src/Revision/evenOdd.java
+++ src/Revision/evenOdd.java
@@ -6,15 +6,15 @@ public class evenOdd {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("enter your first number: ");
-        int num1 = sc.nextInt();
-//        System.out.println("Enter your second number: ");
-//        int num2 = sc.nextInt();
-        if(num1%2==0){
-            System.out.println("the given number is even ");
-        } else{
-            System.out.println("the given number is odd ");
-        }
+//        System.out.println("enter your first number: ");
+//        int num1 = sc.nextInt();
+////        System.out.println("Enter your second number: ");
+////        int num2 = sc.nextInt();
+//        if(num1%2==0){
+//            System.out.println("the given number is even ");
+//        } else{
+//            System.out.println("the given number is odd ");
+//        }
 
 
 
@@ -26,7 +26,16 @@ public class evenOdd {
 
 //        divisble by 5 or not
 
-        System.out.println("Enter your first number");
+        System.out.println("Enter your  number");
+        int num = sc.nextInt();
+        if(num%5==0){
+            System.out.println("the given number is divisble by 5");
+
+        }
+        else{
+            System.out.println("The given number is not divisible by 5");
+        }
+
 
     }
 }
